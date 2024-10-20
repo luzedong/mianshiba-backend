a17a9f1c36985eda9c9b3f106bbb3fd66e924ccf
zedong <zedong_lu@163.com>
2024-10-20 21:43:38 +0800

    init project

---
+++ src/main/java/com/gnodez/mianshiba/controller/PostThumbController.java
@@ -0,0 +1,55 @@
+package com.gnodez.mianshiba.controller;
+
+import com.gnodez.mianshiba.common.BaseResponse;
+import com.gnodez.mianshiba.common.ErrorCode;
+import com.gnodez.mianshiba.common.ResultUtils;
+import com.gnodez.mianshiba.exception.BusinessException;
+import com.gnodez.mianshiba.model.dto.postthumb.PostThumbAddRequest;
+import com.gnodez.mianshiba.model.entity.User;
+import com.gnodez.mianshiba.service.PostThumbService;
+import com.gnodez.mianshiba.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ *
+ *
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+            HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
---
+++ src/main/java/com/gnodez/mianshiba/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.gnodez.mianshiba.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gnodez.mianshiba.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
